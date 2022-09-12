2923acd7b159d8f6d5a897d94b96aa51eb240128
ealpago <alpagoemre@icloud.com>
2022-09-12 17:59:03 +0300

    cardView AutoLayout done

--- TimePad/Constants/ColorConstant.swift
+++ TimePad/Constants/ColorConstant.swift
@@ -11,7 +11,7 @@ import UIKit
 class ColorConstant{
     static let shared = ColorConstant()
     
-    let mainBackgroundColor = UIColor(rgb: 0xE5E5E5)
+    let mainBackgroundColor = UIColor(rgb: 0xFAFAFF)
     let titleTextColor = UIColor(rgb: 0x070417)
     let cardBackgroundColor = UIColor(rgb: 0x070417)
     let cardHourColor = UIColor(rgb: 0xFFFFFF)
--- TimePad/Controllers/Home/HomeViewController.swift
+++ TimePad/Controllers/Home/HomeViewController.swift
@@ -14,19 +14,30 @@ class HomeViewController: UIViewController {
     @IBOutlet private var cardView: UIView?
     @IBOutlet private var cardHourLabel: UILabel?
     @IBOutlet private var cardDetailsButton: UIButton?
+    @IBOutlet private var todayLabel: UILabel?
+    @IBOutlet private var seeAllButton: UIButton?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        setupUI()
+        setupColor()
+        setupCardView()
     }
     
-    
-    func setupUI(){
+    func setupColor(){
         view.backgroundColor = ColorConstant.shared.mainBackgroundColor
         cardView?.backgroundColor = ColorConstant.shared.cardBackgroundColor
         taskLabel?.textColor = ColorConstant.shared.titleTextColor
         cardHourLabel?.textColor = ColorConstant.shared.cardHourColor
+        todayLabel?.textColor = ColorConstant.shared.cardBackgroundColor
+        seeAllButton?.tintColor = ColorConstant.shared.cardBackgroundColor
     }
+    
+    func setupCardView(){
+        cardView?.layer.masksToBounds = true
+        cardView?.layer.cornerRadius = cardView!.frame.width/30.0
+        cardView?.layer.borderWidth = 0.3
+    }
+    
 }
