70e3f7debc5612ef4f55ae67392aca4e3e4fbe7f
ealpago <alpagoemre@icloud.com>
2022-09-16 21:25:44 +0300

    delete - some dummyLines deleted

--- Constants/ColorConstant.swift
+++ Constants/ColorConstant.swift
@@ -25,20 +25,8 @@ enum ColorConstants {
 
 class ColorConstant {
     static let shared = ColorConstant()
-    
-    let mainBackgroundColor = UIColor(rgb: 0xFAFAFF)
-    let titleTextColor = UIColor(rgb: 0x070417)
-    let cardBackgroundColor = UIColor(rgb: 0x070417)
-    let cardHourColor = UIColor(rgb: 0xFFFFFF)
-    
-    let tableViewCellWorkTitleTextColor = UIColor(rgb: 0x000000)
-    let tableViewWorkHourTextColor = UIColor(rgb: 0x4F4F4F)
-    
-    let workCategoryViewColor = UIColor(rgb: 0xFFEFF1)
+
     let workCategoryLabelColor = UIColor(rgb: 0xFD5B71)
-    
-    let RasionProjectViewColor = UIColor(rgb: 0xF5EEFC)
-    let RasionProjectLabelColor = UIColor(rgb: 0x9B51E0)
-    
+  
     let pauseAndQuitButtonsColor = UIColor(rgb: 0xE9E9FF)
 }
