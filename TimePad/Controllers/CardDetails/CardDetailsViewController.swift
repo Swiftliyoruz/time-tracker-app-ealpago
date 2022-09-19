a868811bb6edd905d4a674fbdb6d00b4a67c8ce7
ealpago <alpagoemre@icloud.com>
2022-09-19 18:53:24 +0300

    CardDetailsVC and StoryBoard added

---
+++ TimePad/Controllers/CardDetails/CardDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CardDetailsViewController.swift
+//  TimePad
+//
+//  Created by Emre Alpago on 19.09.2022.
+//
+
+import UIKit
+
+class CardDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
