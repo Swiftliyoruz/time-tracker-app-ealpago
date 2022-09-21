01d48ee28f6ae8f7817ede3b5cb56ed8b6c5208b
ealpago <alpagoemre@icloud.com>
2022-09-21 14:30:40 +0300

    add - AddViewController IBOutlets added

--- TimePad/Controllers/Add/AddViewController.swift
+++ TimePad/Controllers/Add/AddViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class AddViewController: UIViewController {
 
+    @IBOutlet weak var projectNameTextField: UITextField!
+    @IBOutlet weak var mainCategoryButton: UIButton!
+    @IBOutlet weak var secondCategoryButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
