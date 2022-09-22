1a56b254004e562ff1f79cc85a471535330cf0f8
ealpago <alpagoemre@icloud.com>
2022-09-22 15:40:49 +0300

    creating CoreDataClass and CoreDataProperties

---
+++ TimePad/CoreDataModels/Task+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Task+CoreDataClass.swift
+//  TimePad
+//
+//  Created by Emre Alpago on 22.09.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Task)
+public class Task: NSManagedObject {
+
+}
