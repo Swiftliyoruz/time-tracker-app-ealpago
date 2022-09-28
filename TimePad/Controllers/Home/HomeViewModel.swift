3f0e1a2307cbdcb260996308d2581e9d60361ac1
ealpago <alpagoemre@icloud.com>
2022-09-28 20:41:12 +0300

    create - HomeViewModel protocols are created

--- TimePad/Controllers/Home/HomeViewModel.swift
+++ TimePad/Controllers/Home/HomeViewModel.swift
@@ -8,6 +8,24 @@
 import Foundation
 import class UIKit.UIApplication
 
+protocol HomeViewModelDelegate: AnyObject {
+    func setupColor()
+    func setupCardView()
+    func setupTableView()
+    func presentCardDetails()
+}
+
+//MARK: - HomeViewModelInterface
+protocol HomeViewModelInterface {
+    var delegate: HomeViewModelDelegate? { get set }
+
+    var numberOfRowsInSection: Int { get }
+    var heightForHeaderInSection: Double { get }
+
+    func viewDidLoad()
+    func cardDetailsButtonTapped()
+}
+
 final class HomeViewModel {
-    
+
 }
